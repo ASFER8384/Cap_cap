b0355667121085f37c7f3a6d0cec6e41dbfaa808
Asfer <115686272+ASFER8384@users.noreply.github.com>
2023-03-27 16:33:50 +0530

    Update server.js

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ import foodRoutes from "./routes/foodRoute.js";
 import cors from "cors";
 import mongoose from "mongoose";
 import path from "path"
-import {fileUrlToPath} from "url"
+import {fileURLToPath} from "url"
 
 
 
@@ -19,7 +19,7 @@ dotenv.config();
 //databse config
 connectDB();
 
-const __filename = fileUrlToPath(import.meta.url)
+const __filename = fileURLToPath(import.meta.url)
 const __dirname = path.dirname(__filename)
 
 //object app
