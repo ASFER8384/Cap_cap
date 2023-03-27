7cb4fc167ee9201b4257547ab56146a5d8bc5669
Asfer <115686272+ASFER8384@users.noreply.github.com>
2023-03-27 16:11:15 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,56 @@
+import express from "express";
+import colors from "colors";
+import dotenv from "dotenv";
+import morgan from "morgan";
+import connectDB from "./config/db.js";
+import authRoutes from "./routes/authRoute.js";
+import categoryRoutes from "./routes/categoryRoutes.js";
+import foodRoutes from "./routes/foodRoute.js";
+import cors from "cors";
+import mongoose from "mongoose";
+import path from "path"
+import {fileUrlToPath} from "url"
+
+
+
+//configure env
+dotenv.config();
+
+//databse config
+connectDB();
+
+const __filename = fileUrlToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
+
+//object app
+const app = express();
+
+//middelwares
+app.use(cors());
+app.use(express.json());
+app.use(morgan("dev"));
+app.use(express.static(path.join(__dirname, './Client/build')))
+
+//false query
+mongoose.set('strictQuery', false)
+
+//routes
+app.use("/api/v1/auth", authRoutes);
+app.use("/api/v1/category", categoryRoutes);
+app.use("/api/v1/food", foodRoutes);
+
+//rest api
+app.use('*',function (req,res){
+  res.sendFile(path.join(__dirname, './Client/build/index.html'))
+})
+
+//PORT
+const PORT = process.env.PORT || 7000;
+
+//listen run
+app.listen(PORT, () => {
+  console.log(
+    `Server Running on ${process.env.DEV_MODE} mode on port ${PORT}`.bgCyan
+      .white
+  );
+});
